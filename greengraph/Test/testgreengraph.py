d6b5f2cf8b56b20aa391e0183106e944eb7112b3
Ernest Lo <ernest.lo.ii@gmail.com>
2016-01-11 14:17:33 +0000

    first full commit

--- greengraph/Test/testgreengraph.py
+++ greengraph/Test/testgreengraph.py
@@ -3,7 +3,7 @@ import numpy as np
 import unittest
 import yaml
 
-from ..greengraph import Greengraph
+from greengraph import *
 from nose.tools import *
 from mock import Mock, patch
 
@@ -12,8 +12,8 @@ def test_geolocate():
 	    fixtures = yaml.load(fixtures_file)
 	    for fixt in fixtures:
 		    location = fixt.pop('location')    
-		    lats = fixt.pop('lat')
-		    longs = fixt.pop('long')
+		    lat = fixt.pop('lat')
+		    long = fixt.pop('long')
 		    answer = (lat,long)
 	    
 		    Trial = Greengraph(0.0,0.0)
@@ -30,8 +30,6 @@ def test_location_sequence():
 		    step = fixt.pop('steps')  
 		    lat = fixt.pop('lat')
 		    long = fixt.pop('long')		    
-		    
 		    Trial = Greengraph(0.0,0.0)
 		    answer = Trial.location_sequence(Trial.geolocate(orig),Trial.geolocate(dest),step)
-		    assert_equal(answer[fixt][0],lat )
-			assert_equal(answer[fixt][1],long)	
+		    assert(answer[fixt][1]==long and answer[fixt][o]==lat)	
